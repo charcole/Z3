3432fd6cbac88a8c566baf4d0f07447587b7f7e2
charlie <charlie@vyse.me.uk>
2014-09-27 16:49:59 +0100

    More BIOS work

--- combinedata.c
+++ combinedata.c
@@ -2,6 +2,13 @@
 
 unsigned char data[128*1024]={0};
 
+#define CALLBACK_BASE 0x1F80B
+#define INIT_CALLBACK CALLBACK_BASE+0
+#define PRINT_CALLBACK CALLBACK_BASE+5
+#define PRINTCHAR_CALLBACK CALLBACK_BASE+10
+#define PRINTNUM_CALLBACK CALLBACK_BASE+15
+#define READ_CALLBACK CALLBACK_BASE+20
+
 int main()
 {
 	FILE *f;
@@ -20,4 +27,11 @@ int main()
 	f=fopen("rom.z3","wb");
 	fwrite(data, 1, sizeof(data), f);
 	fclose(f);
+
+	printf("Insert this into the Verilog...\n");
+	printf("`define INIT_CALLBACK\t\t'h%02x%02x\n", data[INIT_CALLBACK], data[INIT_CALLBACK+1]);
+	printf("`define PRINT_CALLBACK\t\t'h%02x%02x\n", data[PRINT_CALLBACK], data[PRINT_CALLBACK+1]);
+	printf("`define PRINTCHAR_CALLBACK\t'h%02x%02x\n", data[PRINTCHAR_CALLBACK], data[PRINTCHAR_CALLBACK+1]);
+	printf("`define PRINTNUM_CALLBACK\t'h%02x%02x\n", data[PRINTNUM_CALLBACK], data[PRINTNUM_CALLBACK+1]);
+	printf("`define READ_CALLBACK\t\t'h%02x%02x\n", data[READ_CALLBACK], data[READ_CALLBACK+1]);
 }
